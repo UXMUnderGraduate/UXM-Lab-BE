9027fc4fcbb1faa71425447b08478d31d8c02880
dojinyou <djyou128@gmail.com>
2022-11-27 15:48:48 +0900

    [#1]chore: api prefix 설정 및 server port 환경변수 처리

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function run() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port: number = Number.parseInt(process.env.PORT) || 8080;
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(port);
 }
-bootstrap();
+
+run();

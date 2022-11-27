a0b2912b73019bffc19ce26e1a6cad23664e208c
dojinyou <djyou128@gmail.com>
2022-11-27 15:27:41 +0900

    [#1]fix: orm 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NoticeModule } from './notice/notice.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import typeOrmConfig from './config/type-orm.config';
 
 @Module({
-  imports: [NoticeModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), NoticeModule],
   controllers: [AppController],
   providers: [AppService],
 })

5169bfe0cef7050f9e718221f45fc519c922b9b9
youngcircle-kim <dudghks021121@gmail.com>
2023-05-15 01:53:18 +0900

    feat: xss 방지 helmet 미들웨어 추가

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 async function run() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port: number = Number.parseInt(process.env.PORT) || 8887;
 
   app.use(cookieParser());
+  app.use(helmet());
   app.setGlobalPrefix('api');
 
   await app.listen(port);
